3009551a34d27add4164071ffb3bc6b43e124d99
marcbright90 <brightmarc90gmail.com>
2024-01-03 15:54:30 +0100

    question 8

--- 01_population.py
+++ 01_population.py
@@ -11,7 +11,6 @@ populations = [
     { "id" : 9, "name" : "Edgar" },
     { "id" : 10, "name" : "Erika" },
     { "id" : 11, "name" : "Isaac" },
-    { "id" : 12, "name" : "Amanda" },
     { "id" : 13, "name" : "Brice" },
     { "id" : 14, "name" : "Alice" },
     { "id" : 15, "name" : "Sophia" },
@@ -26,30 +25,25 @@ populations = [
     { "id" : 25, "name" : "Alexis" },
     { "id" : 26, "name" : "Madison" },
     { "id" : 27, "name" : "Nicole" },
-    { "id" : 28, "name" : "Haley" }  
+    { "id" : 28, "name" : "Amanda" },
+    { "id" : 29, "name" : "Haley" }  
 ]
 
+print(len(populations))
 # question 1
-def defineLenChar():
-    for person in populations:
-        person["lenChar"] = len(person["name"])
-
-defineLenChar()
-#print(populations)
+for person in populations:
+    person["lenChar"] = len(person["name"])
 
 # question 2
 from random import randint
-def defRate():
-    for person in populations:
-        person["rate"] = randint(1, 10)
 
-defRate()
-print(populations)
+for person in populations:
+    person["rate"] = randint(1, 100)
 
 # question 3
 fourbestRate = []
 def getFourBestRate():
-    for i in range(10, 1, -1):
+    for i in range(100, 1, -1):
         for person in populations:
             if(person["rate"] == i): fourbestRate.append(person)
             if(len(fourbestRate) == 4): break
@@ -63,17 +57,56 @@ def increaseRate():
         person["rate"] = round(person["rate"] * (1 + 0.1), 2)
 
 increaseRate()
-print(populations)
+#print(populations)
 
 # question 5
-def getRandomPerson():
-    nb = randint(0, len(populations)-1)
-    person = populations[nb]
+def getRandomPerson(): 
+    person = populations[randint(0, len(populations)-1)]
     return person
+
 print(getRandomPerson())
 
 # question 6
 def sortPopulations():
     s = sorted(populations, key=lambda person: person["rate"])
     return s
-print(sortPopulations())
+populations = sortPopulations()
+print(list(map(lambda person: (person["name"], person["rate"]), populations)))
+
+# question 7
+from statistics import median
+def getMedianValue(popList):
+    values = []
+    for person in popList:
+        values.append(person["rate"])
+    medianVal = median(values)
+    return medianVal
+
+mediane = getMedianValue(populations)
+#print(mediane)
+
+# question 8
+def partition(med, popList):
+    p1, p2 = [], []
+    for person in popList:
+        if med <= person["rate"]: p1.append(person)
+        else: p2.append(person)
+    return p1, p2
+
+#calcul des 2 premières parties
+p2, p1 = partition(mediane, populations)
+#calcul des médianes respectives de p1 et p2
+m1 = getMedianValue(p1)
+m2 = getMedianValue(p2)
+
+p1_1, p1_2 = partition(m1, p1)
+p2_1, p2_2 = partition(m2, p2)
+
+print("================================")
+print(p1_2)
+print("========= \n")
+print(p1_1)
+print("========= \n")
+print(p2_2)
+print("========= \n")
+print(p2_1)
